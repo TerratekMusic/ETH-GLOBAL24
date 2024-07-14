5fe3b687aa8f96812bcd018ecffbd25ee20f869a
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-07-13 20:28:13 -0600

    approve

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -19,6 +19,8 @@ import {
   SignlessButtons,
 } from "@/components/ExampleComponents";
 import { Flex, Heading, Input, Button, Text, Box } from "@chakra-ui/react";
+import { Metadata } from "@polkadot/types";
+import { get } from "http";
 
 function Home() {
   const [tokensReceived, setTokensReceived] = useState("");
@@ -41,7 +43,7 @@ function Home() {
   //get USDC balance
 
   const { api } = useApi();
-  const { account } = useAccount();
+  const { accounts, account } = useAccount();
 
   const alert = useAlert();
 
@@ -51,27 +53,114 @@ function Home() {
 
   const Localbalances = fullState.balances || [];
 
-  // Add your programID
+  // USDC program ID
   const programIDFT =
     "0xd8d0206ab4a4d0f26f80a28594e767d62bf1d5ff436c8c79e819a97399b7eaa7";
 
-  // Add your metadata.txt
+  // USDC metadata.txt
   const meta =
     "00010001000000000001030000000107000000000000000108000000a90b3400081466745f696f28496e6974436f6e66696700000c01106e616d65040118537472696e6700011873796d626f6c040118537472696e67000120646563696d616c73080108753800000400000502000800000503000c081466745f696f204654416374696f6e000118104d696e74040010011075313238000000104275726e040010011075313238000100205472616e736665720c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380002001c417070726f7665080108746f14011c4163746f724964000118616d6f756e74100110753132380003002c546f74616c537570706c790004002442616c616e63654f66040014011c4163746f724964000500001000000507001410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004001801205b75383b2033325d0000180000032000000008001c081466745f696f1c46544576656e74000110205472616e736665720c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380000001c417070726f76650c011066726f6d14011c4163746f724964000108746f14011c4163746f724964000118616d6f756e74100110753132380001002c546f74616c537570706c790400100110753132380002001c42616c616e63650400100110753132380003000020081466745f696f3c496f46756e6769626c65546f6b656e00001801106e616d65040118537472696e6700011873796d626f6c040118537472696e67000130746f74616c5f737570706c791001107531323800012062616c616e6365732401505665633c284163746f7249642c2075313238293e000128616c6c6f77616e6365732c01905665633c284163746f7249642c205665633c284163746f7249642c2075313238293e293e000120646563696d616c730801087538000024000002280028000004081410002c00000230003000000408142400";
 
   const metadata = ProgramMetadata.from(meta);
+
+  const handleClick = () => {
+    const programIDFTBond =
+      "0x8004689c0ce66ceac9f323633d8aabdc30c12ea8dd89f494ef367f7aa0b56a3f";
+    // Add your metadata.txt
+    const metaBond =
+      "0002000100000000000105000000010600000000000000000107000000690a38000808696f20496e6974426f6e640000080148737461626c65636f696e5f6164647265737304011c4163746f72496400011470726963651001107531323800000410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004000801205b75383b2033325d000008000003200000000c000c0000050300100000050700140808696f28426f6e64416374696f6e0001041c427579426f6e6404001001107531323800000000180808696f24426f6e644576656e7400011c084f6b0000000c45727200010028426f6e64426f756768740400100110753132380002001c50746f6b656e7304001001107531323800030024426f6e6456616c75650400100110753132380004002c426f6e6442616c616e63650400100110753132380005003450746f6b656e42616c616e6365040010011075313238000600001c0808696f34496f476c6f62616c537461746500002001146f776e657204011c4163746f724964000148737461626c65636f696e5f6164647265737304011c4163746f72496400013c705f746f6b656e5f6164647265737304011c4163746f724964000134626f6e64735f656d6d697465641001107531323800011470726963651001107531323800013076657374696e675f74696d6520010c75363400013c746f74616c5f6465706f736974656410011075313238000130626f6e645f686f6c6465727324017442547265654d61703c4163746f7249642c20426f6e64486f6c6465723e000020000005060024042042547265654d617008044b01040456012800040030000000280808696f28426f6e64486f6c6465720000080124705f62616c616e63651001107531323800011c656d6d697465642c0118537472696e6700002c00000502003000000234003400000408042800";
+
+    const metadataBond = ProgramMetadata.from(metaBond);
+
+    const messageApprove: any = {
+      destination: programIDFT, // programId
+      payload: { Approve: programIDFTBond, inputValue: Number(inputValue) },
+      gasLimit: 8998192450,
+      value: 0,
+    };
+
+    const message: any = {
+      destination: programIDFTBond, // programId
+      payload: { BuyBond: Number(inputValue) },
+      gasLimit: 8998192450,
+      value: 0,
+    };
+
+    const signer = async () => {
+      const localaccount = account?.address;
+      const isVisibleAccount = accounts.some(
+        (visibleAccount) => visibleAccount.address === localaccount
+      );
+
+      if (isVisibleAccount) {
+        // Create a message extrinsic
+        const transferExtrinsicApprove = await api.message.send(
+          messageApprove,
+          metadata
+        );
+        const transferExtrinsic = await api.message.send(message, metadataBond);
+
+        const injector = await web3FromSource(accounts[0].meta.source);
+
+        transferExtrinsicApprove
+          .signAndSend(
+            account?.address ?? alert.error("No account"),
+            { signer: injector.signer },
+            ({ status }) => {
+              if (status.isInBlock) {
+                alert.success(status.asInBlock.toString());
+              } else {
+                console.log("in process");
+                if (status.type === "Finalized") {
+                  alert.success(status.type);
+                }
+              }
+            }
+          )
+          .catch((error: any) => {
+            console.log(":( transaction failed", error);
+          });
+
+        transferExtrinsic
+          .signAndSend(
+            account?.address ?? alert.error("No account"),
+            { signer: injector.signer },
+            ({ status }) => {
+              if (status.isInBlock) {
+                alert.success(status.asInBlock.toString());
+              } else {
+                console.log("in process");
+                if (status.type === "Finalized") {
+                  alert.success(status.type);
+                }
+              }
+            }
+          )
+          .catch((error: any) => {
+            console.log(":( transaction failed", error);
+          });
+      } else {
+        alert.error("Account not available to sign");
+      }
+    };
+    signer();
+
+    console.log("Deposit action performed");
+  };
 
   const getBalance = () => {
     api.programState
       .read({ programId: programIDFT, payload: "" }, metadata)
       .then((result) => {
         setFullState(result.toJSON());
+        // console.log("Full State", fullState);
       })
       .catch(({ message }: Error) => alert.error(message));
 
     Localbalances.some(([address, balances]: any) => {
       if (encodeAddress(address) === account?.address) {
         setBalance(balances);
+        console.log("Balances", balance);
 
         return true;
       }
@@ -81,9 +170,7 @@ function Home() {
 
   useEffect(() => {
     getBalance();
-  }, [account, balance]);
-
-  console.log("Balance", balance);
+  });
 
   return (
     <Flex flexDir="column" justify="center">
@@ -99,7 +186,7 @@ function Home() {
                 value={inputValue} // Controlled component
                 onChange={handleInputChange}
               ></Input>
-              <Button>Buy Bond</Button>
+              <Button onClick={handleClick}>Buy Bond</Button>
             </Flex>
           </Box>
         </Flex>
