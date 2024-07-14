f0649257a8d22ba1ca773f9dae779fada5d06440
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-07-14 01:02:25 -0600

    update

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -13,6 +13,8 @@ import { Flex, Heading, Input, Button, Text, Box } from "@chakra-ui/react";
 import { Metadata } from "@polkadot/types";
 import { get } from "http";
 
+import bgImage from "../../assets/images/Firefly abstract wa.jpg";
+
 function Home() {
   const [tokensReceived, setTokensReceived] = useState("");
 
@@ -55,11 +57,12 @@ function Home() {
   const metadata = ProgramMetadata.from(meta);
 
   const handleClick = () => {
+    //BOND program ID
     const programIDFTBond =
-      "0x8004689c0ce66ceac9f323633d8aabdc30c12ea8dd89f494ef367f7aa0b56a3f";
+      "0x228c61da585fb256b2761c9250b0588dcdd11458035f23731d51c945a05a9231";
     // Add your metadata.txt
     const metaBond =
-      "0002000100000000000105000000010600000000000000000107000000690a38000808696f20496e6974426f6e640000080148737461626c65636f696e5f6164647265737304011c4163746f72496400011470726963651001107531323800000410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004000801205b75383b2033325d000008000003200000000c000c0000050300100000050700140808696f28426f6e64416374696f6e0001041c427579426f6e6404001001107531323800000000180808696f24426f6e644576656e7400011c084f6b0000000c45727200010028426f6e64426f756768740400100110753132380002001c50746f6b656e7304001001107531323800030024426f6e6456616c75650400100110753132380004002c426f6e6442616c616e63650400100110753132380005003450746f6b656e42616c616e6365040010011075313238000600001c0808696f34496f476c6f62616c537461746500002001146f776e657204011c4163746f724964000148737461626c65636f696e5f6164647265737304011c4163746f72496400013c705f746f6b656e5f6164647265737304011c4163746f724964000134626f6e64735f656d6d697465641001107531323800011470726963651001107531323800013076657374696e675f74696d6520010c75363400013c746f74616c5f6465706f736974656410011075313238000130626f6e645f686f6c6465727324017442547265654d61703c4163746f7249642c20426f6e64486f6c6465723e000020000005060024042042547265654d617008044b01040456012800040030000000280808696f28426f6e64486f6c6465720000080124705f62616c616e63651001107531323800011c656d6d697465642c0118537472696e6700002c00000502003000000234003400000408042800";
+      "0002000100000000000105000000010600000000000000000107000000090b38000808696f20496e6974426f6e6400000c0148737461626c65636f696e5f6164647265737304011c4163746f724964000130626f6e645f6164647265737304011c4163746f72496400011470726963651001107531323800000410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004000801205b75383b2033325d000008000003200000000c000c0000050300100000050700140808696f28426f6e64416374696f6e0001041c427579426f6e6404001001107531323800000000180808696f24426f6e644576656e7400011c084f6b0000000c45727200010028426f6e64426f756768740400100110753132380002001c50746f6b656e7304001001107531323800030024426f6e6456616c75650400100110753132380004002c426f6e6442616c616e63650400100110753132380005003450746f6b656e42616c616e6365040010011075313238000600001c0808696f10426f6e6400002401146f776e657204011c4163746f724964000148737461626c65636f696e5f6164647265737304011c4163746f724964000130626f6e645f6164647265737304011c4163746f72496400013c705f746f6b656e5f6164647265737304011c4163746f724964000134626f6e64735f656d69747465641001107531323800011470726963651001107531323800013476657374696e675f626c6f636b20010c75333200013c746f74616c5f6465706f736974656410011075313238000130626f6e645f686f6c6465727324017442547265654d61703c4163746f7249642c20426f6e64486f6c6465723e000020000005050024042042547265654d617008044b01040456012800040030000000280808696f28426f6e64486f6c6465720000080124705f62616c616e63651001107531323800011c656d69747465642c0110626f6f6c00002c00000500003000000234003400000408042800";
 
     const metadataBond = ProgramMetadata.from(metaBond);
 
@@ -104,25 +107,6 @@ function Home() {
           .catch((error: any) => {
             console.log(":( transaction failed", error);
           });
-
-        // transferExtrinsic
-        //   .signAndSend(
-        //     account?.address ?? alert.error("No account"),
-        //     { signer: injector.signer },
-        //     ({ status }) => {
-        //       if (status.isInBlock) {
-        //         alert.success(status.asInBlock.toString());
-        //       } else {
-        //         console.log("in process");
-        //         if (status.type === "Finalized") {
-        //           alert.success(status.type);
-        //         }
-        //       }
-        //     }
-        //   )
-        //   .catch((error: any) => {
-        //     console.log(":( transaction failed", error);
-        //   });
       } else {
         alert.error("Account not available to sign");
       }
@@ -194,7 +178,7 @@ function Home() {
     };
     signer();
 
-    console.log("Approve action performed");
+    console.log("BUY action performed");
   };
 
   const getBalance = () => {
@@ -223,9 +207,22 @@ function Home() {
 
   return (
     <Flex flexDir="column" justify="center">
-      <Heading alignSelf="center">Buy Bond</Heading>
+      <Flex pt="4rem" justify="center" bgImage={bgImage} w="100%" h="18rem">
+        {" "}
+        <Heading color="black" mt="2rem" alignSelf="center">
+          Buy Bond
+        </Heading>
+      </Flex>
+
       <Flex alignSelf="center">
-        <Flex mt="4rem" justify="center">
+        <Flex
+          p="2rem"
+          borderWidth="1px"
+          borderColor="grey"
+          mt="2rem"
+          justify="center"
+          borderRadius="2rem"
+        >
           <Box>
             <Text>Input the amount of USDC that you want to spend</Text>
             <Text ml="12rem">USDC Available:{balance}</Text>
@@ -246,12 +243,24 @@ function Home() {
           <Text>Current market price: 4.1 USDC</Text>
           <Text>Current bond price: 3.0 USDC</Text>
           <Text>Bond Disscount: 30%</Text>
-          <Text>Vesting Period: 7 blocks</Text>
+          <Text>Vesting Period: 15 blocks</Text>
         </Flex>
       </Flex>
-      <Flex alignSelf="center" flexDir="column" mt="4rem" justify="center">
-        <Heading>You receive</Heading>
-        <Text fontSize="1.3rem">{tokensReceived} pTokens</Text>
+      <Flex
+        p="1rem"
+        borderWidth="1px"
+        borderColor="grey"
+        bgColor="#035BFE"
+        alignSelf="center"
+        flexDir="column"
+        mt="4rem"
+        justify="center"
+        borderRadius="2rem"
+      >
+        <Heading color="white">You receive</Heading>
+        <Text color="white" fontSize="1.3rem">
+          {tokensReceived} pTokens
+        </Text>
       </Flex>
     </Flex>
   );
